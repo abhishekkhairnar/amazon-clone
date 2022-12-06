80f7fe21444f64b8f13b24c427ab962f37287655
abhishekkhairnar <khairnarabhi25@gmail.com>
2022-12-07 00:38:47 +0530

    Working on Home section

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Header.css';
+import SearchIcon from '@mui/icons-material/Search';
+import  ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 function Header() {
   return (
         <div className="header">
             <img src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazon" className="header__logo" />
             <div className="header__search">
                 <input type="text" className="header__searchInput" />
-                {/* logo */}
+                <SearchIcon className="header__searchIcon" />
             </div>
             <div className="header__nav">
                 <div className="header__option">
@@ -18,7 +20,7 @@ function Header() {
                 <div className="header__option">
                     <span className="header__optionLineOne">Returns
                     </span>
-                    <span className="header__optionLineTwo">Orders
+                    <span className="header__optionLineTwo">& Orders
                     </span>
                 </div>
                 <div className="header__option">
@@ -27,11 +29,9 @@ function Header() {
                     <span className="header__optionLineTwo">Prime
                     </span>
                 </div>
-                <div className="header__option">
-                    <span className="header__optionLineOne">Hello Guest
-                    </span>
-                    <span className="header__optionLineTwo">Sign In
-                    </span>
+                <div className="header__optionBasket">
+                    <ShoppingBasketIcon/>
+                    <span className="header__optionLineTwo header__basketCount">0</span>
                 </div>
             </div>
         </div>
---
+++ src/Product.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Product.css'
+function Product() {
+  return (
+    <div className="product">
+        <div className="product__info">
+            <p>The Lean Startup</p>
+            <p className="product__price">
+                <small>$</small>
+                <strong>19.99</strong>
+            </p>
+            <div className="product__rating">
+                <p>⭐</p>
+                <p>⭐</p>
+                <p>⭐</p>
+                <p>⭐</p>
+            </div>
+        </div>
+            <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" alt="" />
+
+            <button>Add to Basket </button>
+    </div>
+  )
+}
+
+export default Product
